9fd013da0efd80dad1d04e7d0834b9159a478bf0
Nic Cheneweth <nchenewe@thoughtworks.com>
2019-01-29 18:53:01 -0600

    iterating

--- test/integration/default/tf_aws_state_bucket.rb
+++ test/integration/default/tf_aws_state_bucket.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-describe aws_s3_bucket('feedyard-ci-test-bucket') do
+describe aws_s3_bucket('feedyard-ci-tf-state') do
   it { should exist }
   it { should have_default_encryption_enabled }
 end
